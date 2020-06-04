53de931d0888d501435904ad449aa6c3e96ca7be
Maria Connie Sanchez <msnchz04@gmail.com>
2020-06-04 20:35:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase= "I LOVE YOU GRANDMA!")
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase != phrase.downcase
+  elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
     
     else phrase == phrase.upcase
